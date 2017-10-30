a0504d6fcd1d8f2253497b00946e8c0fd9e43835
Wade Arnold <wade@wadearnold.com>
2017-10-30 09:57:25 -0600

    Test is not used - dead code

--- batchWEB_test.go
+++ batchWEB_test.go
@@ -36,23 +36,6 @@ func mockBatchWEB() *BatchWEB {
 	return mockBatch
 }
 
-// A Batch web can only have one addendum per entry detail
-func TestBatchWEBAddendumCount(t *testing.T) {
-	mockBatch := mockBatchWEB()
-	// Adding a second addenda to the mock entry
-	mockBatch.GetEntries()[0].AddAddenda(mockAddenda())
-
-	if err := mockBatch.Validate(); err != nil {
-		if e, ok := err.(*BatchError); ok {
-			if e.FieldName != "EntryAddendaCount" {
-				t.Errorf("%T: %s", err, err)
-			}
-		} else {
-			t.Errorf("%T: %s", err, err)
-		}
-	}
-}
-
 // No more than 1 batch per entry detail record can exist
 func TestBatchWebAddenda(t *testing.T) {
 	mockBatch := mockBatchWEB()
