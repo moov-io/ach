c24e9e29162d989afd2347f6fdf0813e9da9f252
Brooke E Kline Jr <brooke.kline@moov.io>
2018-06-13 17:43:20 -0400

    build fail
    build fail

--- batchPOS_test.go
+++ batchPOS_test.go
@@ -6,21 +6,6 @@ package ach
 
 import "testing"
 
-func mockAddenda02() *Addenda02 {
-	addenda02 := NewAddenda02()
-	addenda02.ReferenceInformationOne = "REFONEA"
-	addenda02.ReferenceInformationTwo = "REF"
-	addenda02.TerminalIdentificationCode = "TERM02"
-	addenda02.TransactionSerialNumber = "100049"
-	addenda02.TransactionDate = "0612"
-	addenda02.AuthorizationCodeOrExpireDate = "123456"
-	addenda02.TerminalLocation = "Target Store 0049"
-	addenda02.TerminalCity = "PHILADELPHIA"
-	addenda02.TerminalState = "PA"
-	addenda02.TraceNumber = 91012980000088
-	return addenda02
-}
-
 // mockBatchPOSHeader creates a BatchPOS BatchHeader
 func mockBatchPOSHeader() *BatchHeader {
 	bh := NewBatchHeader()
@@ -31,32 +16,6 @@ func mockBatchPOSHeader() *BatchHeader {
 	bh.CompanyEntryDescription = "ACH POS"
 	bh.ODFIIdentification = "12104288"
 	return bh
-}
-
-// mockPOSEntryDetail creates a BatchPOS EntryDetail
-func mockPOSEntryDetail() *EntryDetail {
-	entry := NewEntryDetail()
-	entry.TransactionCode = 27
-	entry.SetRDFI("231380104")
-	entry.DFIAccountNumber = "744-5678-99"
-	entry.Amount = 25000
-	entry.IdentificationNumber = "45689033"
-	entry.SetReceivingCompany("ABC Company")
-	entry.SetTraceNumber(mockBatchPOSHeader().ODFIIdentification, 123)
-	entry.DiscretionaryData = "01"
-	entry.Category = CategoryForward
-	return entry
-}
-
-// mockBatchPOS creates a BatchPOS
-func mockBatchPOS() *BatchPOS {
-	mockBatch := NewBatchPOS(mockBatchPOSHeader())
-	mockBatch.AddEntry(mockPOSEntryDetail())
-	mockBatch.GetEntries()[0].AddAddenda(mockAddenda02())
-	if err := mockBatch.Create(); err != nil {
-		panic(err)
-	}
-	return mockBatch
 }
 
 // testBatchPOSHeader creates a BatchPOS BatchHeader
