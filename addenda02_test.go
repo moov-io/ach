10e645eb7d3e3bcb83c0daabdf692493dcda1d63
Brooke E Kline Jr <brooke.kline@moov.io>
2018-06-13 18:01:02 -0400

    fieldInclusion tests
    fieldInclusion tests

--- addenda02_test.go
+++ addenda02_test.go
@@ -29,3 +29,187 @@ func TestMockAddenda02(t *testing.T) {
 		t.Error("mockAddenda02 does not validate and will break other tests")
 	}
 }
+
+func testAddenda02RecordType(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.recordType = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02RecordType(t *testing.T) {
+	testAddenda02RecordType(t)
+}
+
+func BenchmarkAddenda02FieldInclusionRecordType(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02RecordType(b)
+	}
+}
+
+func testAddenda02TypeCode(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.typeCode = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TypeCode(t *testing.T) {
+	testAddenda02TypeCode(t)
+}
+
+func BenchmarkAddenda02TypeCode(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TypeCode(b)
+	}
+}
+
+func testAddenda02TerminalIdentificationCode(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.TerminalIdentificationCode = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TerminalIdentificationCode(t *testing.T) {
+	testAddenda02TerminalIdentificationCode(t)
+}
+
+func BenchmarkAddenda02TerminalIdentificationCode(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TerminalIdentificationCode(b)
+	}
+}
+
+func testAddenda02TransactionSerialNumber(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.TransactionSerialNumber = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TransactionSerialNumber(t *testing.T) {
+	testAddenda02TransactionSerialNumber(t)
+}
+
+func BenchmarkAddenda02TransactionSerialNumber(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TransactionSerialNumber(b)
+	}
+}
+
+func testAddenda02TransactionDate(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.TransactionDate = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TransactionDate(t *testing.T) {
+	testAddenda02TransactionDate(t)
+}
+
+func BenchmarkAddenda02TransactionDate(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TransactionDate(b)
+	}
+}
+
+func testAddenda02TerminalLocation(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.TerminalLocation = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TerminalLocation(t *testing.T) {
+	testAddenda02TerminalLocation(t)
+}
+
+func BenchmarkAddenda02TerminalLocation(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TerminalLocation(b)
+	}
+}
+
+func testAddenda02TerminalCity(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.TerminalCity = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TerminalCity(t *testing.T) {
+	testAddenda02TerminalCity(t)
+}
+
+func BenchmarkAddenda02TerminalCity(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TerminalCity(b)
+	}
+}
+
+func testAddenda02TerminalState(t testing.TB) {
+	addenda02 := mockAddenda02()
+	addenda02.TerminalState = ""
+	if err := addenda02.Validate(); err != nil {
+		if e, ok := err.(*FieldError); ok {
+			if e.Msg != msgFieldInclusion {
+				t.Errorf("%T: %s", err, err)
+			}
+		}
+	}
+}
+
+func TestAddenda02TerminalState(t *testing.T) {
+	testAddenda02TerminalState(t)
+}
+
+func BenchmarkAddenda02TerminalState(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testAddenda02TerminalState(b)
+	}
+}
