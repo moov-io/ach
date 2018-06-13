f46e10556e573bfe34f0bb2ff9852f3f2394c7c8
Brooke E Kline Jr <brooke.kline@moov.io>
2018-06-13 17:48:33 -0400

    add tests
    Add additional tests to batchPOS_test.go

--- batchPOS_test.go
+++ batchPOS_test.go
@@ -18,6 +18,47 @@ func mockBatchPOSHeader() *BatchHeader {
 	return bh
 }
 
+// mockPOSEntryDetail creates a BatchPOS EntryDetail
+func mockPOSEntryDetail() *EntryDetail {
+	entry := NewEntryDetail()
+	entry.TransactionCode = 27
+	entry.SetRDFI("231380104")
+	entry.DFIAccountNumber = "744-5678-99"
+	entry.Amount = 25000
+	entry.IdentificationNumber = "45689033"
+	entry.SetReceivingCompany("ABC Company")
+	entry.SetTraceNumber(mockBatchPOSHeader().ODFIIdentification, 123)
+	entry.DiscretionaryData = "01"
+	entry.Category = CategoryForward
+	return entry
+}
+
+func mockAddenda02() *Addenda02 {
+	addenda02 := NewAddenda02()
+	addenda02.ReferenceInformationOne = "REFONEA"
+	addenda02.ReferenceInformationTwo = "REF"
+	addenda02.TerminalIdentificationCode = "TERM02"
+	addenda02.TransactionSerialNumber = "100049"
+	addenda02.TransactionDate = "0612"
+	addenda02.AuthorizationCodeOrExpireDate = "123456"
+	addenda02.TerminalLocation = "Target Store 0049"
+	addenda02.TerminalCity = "PHILADELPHIA"
+	addenda02.TerminalState = "PA"
+	addenda02.TraceNumber = 91012980000088
+	return addenda02
+}
+
+// mockBatchPOS creates a BatchPOS
+func mockBatchPOS() *BatchPOS {
+	mockBatch := NewBatchPOS(mockBatchPOSHeader())
+	mockBatch.AddEntry(mockPOSEntryDetail())
+	mockBatch.GetEntries()[0].AddAddenda(mockAddenda02())
+	if err := mockBatch.Create(); err != nil {
+		panic(err)
+	}
+	return mockBatch
+}
+
 // testBatchPOSHeader creates a BatchPOS BatchHeader
 func testBatchPOSHeader(t testing.TB) {
 	batch, _ := NewBatch(mockBatchPOSHeader())
@@ -37,5 +78,201 @@ func BenchmarkBatchPOSHeader(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		testBatchPOSHeader(b)
+	}
+}
+
+// testBatchPOSCreate validates BatchPOS create
+func testBatchPOSCreate(t testing.TB) {
+	mockBatch := mockBatchPOS()
+	mockBatch.Create()
+	if err := mockBatch.Validate(); err != nil {
+		t.Errorf("%T: %s", err, err)
+	}
+}
+
+// TestBatchPOSCreate tests validating BatchPOS create
+func TestBatchPOSCreate(t *testing.T) {
+	testBatchPOSCreate(t)
+}
+
+// BenchmarkBatchPOSCreate benchmarks validating BatchPOS create
+func BenchmarkBatchPOSCreate(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSCreate(b)
+	}
+}
+
+// testBatchPOSStandardEntryClassCode validates BatchPOS create for an invalid StandardEntryClassCode
+func testBatchPOSStandardEntryClassCode(t testing.TB) {
+	mockBatch := mockBatchPOS()
+	mockBatch.Header.StandardEntryClassCode = "WEB"
+	mockBatch.Create()
+	if err := mockBatch.Validate(); err != nil {
+		if e, ok := err.(*BatchError); ok {
+			if e.FieldName != "StandardEntryClassCode" {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestBatchPOSStandardEntryClassCode tests validating BatchPOS create for an invalid StandardEntryClassCode
+func TestBatchPOSStandardEntryClassCode(t *testing.T) {
+	testBatchPOSStandardEntryClassCode(t)
+}
+
+// BenchmarkBatchPOSStandardEntryClassCode benchmarks validating BatchPOS create for an invalid StandardEntryClassCode
+func BenchmarkBatchPOSStandardEntryClassCode(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSStandardEntryClassCode(b)
+	}
+}
+
+// testBatchPOSServiceClassCodeEquality validates service class code equality
+func testBatchPOSServiceClassCodeEquality(t testing.TB) {
+	mockBatch := mockBatchPPD()
+	mockBatch.GetControl().ServiceClassCode = 220
+	if err := mockBatch.Validate(); err != nil {
+		if e, ok := err.(*BatchError); ok {
+			if e.FieldName != "ServiceClassCode" {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestBatchPOSServiceClassCodeEquality tests validating service class code equality
+func TestBatchPOSServiceClassCodeEquality(t *testing.T) {
+	testBatchPOSServiceClassCodeEquality(t)
+}
+
+// BenchmarkBatchPOSServiceClassCodeEquality benchmarks validating service class code equality
+func BenchmarkBatchPOSServiceClassCodeEquality(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSServiceClassCodeEquality(b)
+	}
+}
+
+// testBatchPOSAddendaCount validates BatchPOS Addendum count of 2
+func testBatchPOSAddendaCount(t testing.TB) {
+	mockBatch := mockBatchPOS()
+	mockBatch.GetEntries()[0].AddAddenda(mockAddenda02())
+	mockBatch.Create()
+	if err := mockBatch.Validate(); err != nil {
+		if e, ok := err.(*BatchError); ok {
+			if e.FieldName != "Addendum" {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestBatchPOSAddendaCount tests validating BatchPOS Addendum count of 2
+func TestBatchPOSAddendaCount(t *testing.T) {
+	testBatchPOSAddendaCount(t)
+}
+
+// BenchmarkBatchPOSAddendaCount benchmarks validating BatchPOS Addendum count of 2
+func BenchmarkBatchPOSAddendaCount(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSAddendaCount(b)
+	}
+}
+
+// testBatchPOSAddendaCountZero validates Addendum count of 0
+func testBatchPOSAddendaCountZero(t testing.TB) {
+	mockBatch := NewBatchPOS(mockBatchPOSHeader())
+	mockBatch.AddEntry(mockPOSEntryDetail())
+	if err := mockBatch.Create(); err != nil {
+		if e, ok := err.(*BatchError); ok {
+			if e.FieldName != "Addendum" {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestBatchPOSAddendaCountZero tests validating Addendum count of 0
+func TestBatchPOSAddendaCountZero(t *testing.T) {
+	testBatchPOSAddendaCountZero(t)
+}
+
+// BenchmarkBatchPOSAddendaCountZero benchmarks validating Addendum count of 0
+func BenchmarkBatchPOSAddendaCountZero(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSAddendaCountZero(b)
+	}
+}
+
+// testBatchPOSInvalidAddendum validates Addendum must be Addenda02
+func testBatchPOSInvalidAddendum(t testing.TB) {
+	mockBatch := NewBatchPOS(mockBatchPOSHeader())
+	mockBatch.AddEntry(mockPOSEntryDetail())
+	mockBatch.GetEntries()[0].AddAddenda(mockAddenda05())
+	if err := mockBatch.Create(); err != nil {
+		if e, ok := err.(*BatchError); ok {
+			if e.FieldName != "Addendum" {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestBatchPOSInvalidAddendum tests validating Addendum must be Addenda02
+func TestBatchPOSInvalidAddendum(t *testing.T) {
+	testBatchPOSInvalidAddendum(t)
+}
+
+// BenchmarkBatchPOSInvalidAddendum benchmarks validating Addendum must be Addenda02
+func BenchmarkBatchPOSInvalidAddendum(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSInvalidAddendum(b)
+	}
+}
+
+// testBatchPOSInvalidAddenda validates Addendum must be Addenda02
+func testBatchPOSInvalidAddenda(t testing.TB) {
+	mockBatch := NewBatchPOS(mockBatchPOSHeader())
+	mockBatch.AddEntry(mockPOSEntryDetail())
+	addenda02 := mockAddenda02()
+	addenda02.recordType = "63"
+	mockBatch.GetEntries()[0].AddAddenda(addenda02)
+	if err := mockBatch.Create(); err != nil {
+		if e, ok := err.(*BatchError); ok {
+			if e.FieldName != "recordType" {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestBatchPOSInvalidAddenda tests validating Addendum must be Addenda02
+func TestBatchPOSInvalidAddenda(t *testing.T) {
+	testBatchPOSInvalidAddenda(t)
+}
+
+// BenchmarkBatchPOSInvalidAddenda benchmarks validating Addendum must be Addenda02
+func BenchmarkBatchPOSInvalidAddenda(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		testBatchPOSInvalidAddenda(b)
 	}
 }
