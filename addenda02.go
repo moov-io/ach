b680dcc1b6cb2c0298696f8943beab9c4edc2bcd
Brooke E Kline Jr <brooke.kline@moov.io>
2018-06-13 15:23:26 -0400

    # 208 Support of SEC Code POS
    Add batchPOS
    Add addenda02

---
+++ addenda02.go
@@ -0,0 +1,215 @@
+// Copyright 2018 The ACH Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package ach
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Addenda02 is a Addendumer addenda which provides business transaction information for Addenda Type
+// Code 02 in a machine readable format. It is usually formatted according to ANSI, ASC, X12 Standard.
+type Addenda02 struct {
+	//ToDo: Verify which fields should be omitempty
+	//ToDo: Add mor descriptive comments
+	// ID is a client defined string used as a reference to this record.
+	ID string `json:"id"`
+	// RecordType defines the type of record in the block. entryAddenda02 Pos 7
+	recordType string
+	// TypeCode Addenda02 types code '02'
+	typeCode string
+	// ReferenceInformationOne
+	ReferenceInformationOne string `json:"referenceInformationOne"`
+	// ReferenceInformationTwo
+	ReferenceInformationTwo string `json:"referenceInformationTwo"`
+	// TerminalIdentificationCode
+	TerminalIdentificationCode string `json:"terminalIdentificationCode"`
+	// TransactionSerialNumber
+	TransactionSerialNumber string `json:"transactionSerialNumber"`
+	// TransactionDate MMDD
+	TransactionDate string `json:"transactionDate"`
+	// TraceNumber matches the Entry Detail Trace Number of the entry being returned.
+	// AuthorizationCodeOrExpireDate
+	AuthorizationCodeOrExpireDate string `json:"authorizationCodeOrExpireDate"`
+	// Terminal Location
+	TerminalLocation string `json:"terminalLocation"`
+	// TerminalCity
+	TerminalCity string `json:"terminalCity"`
+	// TerminalState
+	TerminalState string `json:"terminalState"`
+	// TraceNumber matches the Entry Detail Trace Number of the entry being returned.
+	TraceNumber int `json:"traceNumber,omitempty"`
+	// validator is composed for data validation
+	validator
+	// converters is composed for ACH to GoLang Converters
+	converters
+}
+
+// NewAddenda02 returns a new Addenda02 with default values for none exported fields
+func NewAddenda02() *Addenda02 {
+	addenda02 := new(Addenda02)
+	addenda02.recordType = "7"
+	addenda02.typeCode = "02"
+	return addenda02
+}
+
+// Parse takes the input record string and parses the Addenda02 values
+func (addenda02 *Addenda02) Parse(record string) {
+	// 1-1 Always "7"
+	addenda02.recordType = "7"
+	// 2-3 Always 02
+	addenda02.typeCode = record[1:3]
+	// 4-10 Based on the information entered (04-10) 7 alphanumeric
+	addenda02.ReferenceInformationOne = strings.TrimSpace(record[3:10])
+	// 11-13 Based on the information entered (11-13) 3 alphanumeric
+	addenda02.ReferenceInformationTwo = strings.TrimSpace(record[10:13])
+	// 14-19
+	addenda02.TerminalIdentificationCode = strings.TrimSpace(record[13:19])
+	// 20-25
+	addenda02.TransactionSerialNumber = strings.TrimSpace(record[19:25])
+	// 26-29
+	addenda02.TransactionDate = strings.TrimSpace(record[25:29])
+	// 30-35
+	addenda02.AuthorizationCodeOrExpireDate = strings.TrimSpace(record[29:35])
+	// 36-62
+	addenda02.TerminalLocation = strings.TrimSpace(record[35:62])
+	// 63-77
+	addenda02.TerminalCity = strings.TrimSpace(record[62:77])
+	// 78-79
+	addenda02.TerminalState = strings.TrimSpace(record[77:79])
+	// 80-94
+	addenda02.TraceNumber = addenda02.parseNumField(record[79:94])
+}
+
+// String writes the Addenda02 struct to a 94 character string.
+func (addenda02 *Addenda02) String() string {
+	return fmt.Sprintf("%v%v%v%v%v%v%v%v%v%v%v%v",
+		addenda02.recordType,
+		addenda02.typeCode,
+		addenda02.ReferenceInformationOneField(),
+		addenda02.ReferenceInformationTwoField(),
+		addenda02.TerminalIdentificationCodeField(),
+		addenda02.TransactionSerialNumberField(),
+		// ToDo: Follow up on best way to get TransactionDate - should it be treated as an alpha field
+		addenda02.TransactionDateField(),
+		addenda02.AuthorizationCodeOrExpireDateField(),
+		addenda02.TerminalLocationField(),
+		addenda02.TerminalCityField(),
+		addenda02.TerminalStateField(),
+		addenda02.TraceNumberField(),
+	)
+}
+
+// Validate performs NACHA format rule checks on the record and returns an error if not Validated
+// The first error encountered is returned and stops that parsing.
+func (addenda02 *Addenda02) Validate() error {
+	if err := addenda02.fieldInclusion(); err != nil {
+		return err
+	}
+	if addenda02.recordType != "7" {
+		msg := fmt.Sprintf(msgRecordType, 7)
+		return &FieldError{FieldName: "recordType", Value: addenda02.recordType, Msg: msg}
+	}
+	if err := addenda02.isTypeCode(addenda02.typeCode); err != nil {
+		return &FieldError{FieldName: "TypeCode", Value: addenda02.typeCode, Msg: err.Error()}
+	}
+	// Type Code must be 02
+	// ToDo: Evaluate if Addenda05 and Addenda99 should be modified to validate on 05 and 99
+	if addenda02.typeCode != "02" {
+		return &FieldError{FieldName: "TypeCode", Value: addenda02.typeCode, Msg: msgAddendaTypeCode}
+	}
+	// ToDo: Validation for TransactionDate MMDD
+	return nil
+}
+
+// fieldInclusion validate mandatory fields are not default values. If fields are
+// invalid the ACH transfer will be returned.
+
+// ToDo: check if we should do fieldInclusion or validate on required fields
+
+func (addenda02 *Addenda02) fieldInclusion() error {
+	if addenda02.recordType == "" {
+		return &FieldError{FieldName: "recordType", Value: addenda02.recordType, Msg: msgFieldInclusion}
+	}
+	if addenda02.typeCode == "" {
+		return &FieldError{FieldName: "TypeCode", Value: addenda02.typeCode, Msg: msgFieldInclusion}
+	}
+	if addenda02.TerminalIdentificationCode == "" {
+		return &FieldError{FieldName: "TerminalIdentificationCode", Value: addenda02.TerminalIdentificationCode, Msg: msgFieldInclusion}
+	}
+	if addenda02.TransactionSerialNumber == "" {
+		return &FieldError{FieldName: "TransactionSerialNumber", Value: addenda02.TransactionSerialNumber, Msg: msgFieldInclusion}
+	}
+	if addenda02.TransactionDate == "" {
+		return &FieldError{FieldName: "TransactionDate", Value: addenda02.TransactionDate, Msg: msgFieldInclusion}
+	}
+	if addenda02.TerminalLocation == "" {
+		return &FieldError{FieldName: "TerminalLocation", Value: addenda02.TerminalLocation, Msg: msgFieldInclusion}
+	}
+	if addenda02.TerminalCity == "" {
+		return &FieldError{FieldName: "TerminalCity", Value: addenda02.TerminalCity, Msg: msgFieldInclusion}
+	}
+	if addenda02.TerminalState == "" {
+		return &FieldError{FieldName: "TerminalState", Value: addenda02.TerminalState, Msg: msgFieldInclusion}
+	}
+	// ToDo: Determine if TraceNumber fieldInclusion is necessary
+	return nil
+}
+
+// TypeCode Defines the specific explanation and format for the addenda02 information
+func (addenda02 *Addenda02) TypeCode() string {
+	return addenda02.typeCode
+}
+
+// ReferenceInformationOneField returns a space padded ReferenceInformationOne string
+func (addenda02 *Addenda02) ReferenceInformationOneField() string {
+	return addenda02.alphaField(addenda02.ReferenceInformationOne, 7)
+}
+
+// ReferenceInformationTwoField returns a space padded ReferenceInformationTwo string
+func (addenda02 *Addenda02) ReferenceInformationTwoField() string {
+	return addenda02.alphaField(addenda02.ReferenceInformationOne, 3)
+}
+
+// TerminalIdentificationCodeField returns a space padded TerminalIdentificationCode string
+func (addenda02 *Addenda02) TerminalIdentificationCodeField() string {
+	return addenda02.alphaField(addenda02.TerminalIdentificationCode, 6)
+}
+
+// TransactionSerialNumberField returns a zero padded TransactionSerialNumber string
+func (addenda02 *Addenda02) TransactionSerialNumberField() string {
+	return addenda02.alphaField(addenda02.TransactionSerialNumber, 6)
+}
+
+// TransactionDateField returns TransactionDate MMDD string
+func (addenda02 *Addenda02) TransactionDateField() string {
+	//ToDo: see about padding
+	return addenda02.TransactionDate
+}
+
+// AuthorizationCodeOrExpireDateField returns a space padded AuthorizationCodeOrExpireDate string
+func (addenda02 *Addenda02) AuthorizationCodeOrExpireDateField() string {
+	return addenda02.alphaField(addenda02.AuthorizationCodeOrExpireDate, 6)
+}
+
+//TerminalLocationField returns a space padded TerminalLocation string
+func (addenda02 *Addenda02) TerminalLocationField() string {
+	return addenda02.alphaField(addenda02.TerminalLocation, 27)
+}
+
+//TerminalCityField returns a space padded TerminalCity string
+func (addenda02 *Addenda02) TerminalCityField() string {
+	return addenda02.alphaField(addenda02.TerminalCity, 15)
+}
+
+//TerminalStateField returns a space padded TerminalState string
+func (addenda02 *Addenda02) TerminalStateField() string {
+	return addenda02.alphaField(addenda02.TerminalState, 2)
+}
+
+// TraceNumberField returns a space padded traceNumber string
+func (addenda02 *Addenda02) TraceNumberField() string {
+	return addenda02.numericField(addenda02.TraceNumber, 15)
+}
---
+++ batchPOS.go
@@ -0,0 +1,109 @@
+// Copyright 2018 The ACH Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package ach
+
+import "fmt"
+
+// BatchPOS holds the BatchHeader and BatchControl and all EntryDetail for POS Entries.
+//
+// A POS Entry is a debit Entry initiated at an “electronic terminal” to a Consumer
+// Account of the Receiver to pay an obligation incurred in a point- of-sale
+// transaction, or to effect a point-of-sale terminal cash withdrawal.
+//
+// Point-of-Sale Entries (POS) are ACH debit entries typically initiated by the use
+// of a merchant-issued plastic card to pay an obligation at the point-of-sale. Much
+// like a financial institution issued debit card, the merchant- issued debit card is
+// swiped at the point-of-sale and approved for use; however, the authorization only
+// verifies the card is open, active and within the card’s limits—it does not verify
+// the Receiver’s account balance or debit the account at the time of the purchase.
+// Settlement of the transaction moves from the card network to the ACH Network through
+// the creation of a POS entry by the card issuer to debit the Receiver’s account.
+type BatchPOS struct {
+	batch
+}
+
+var msgBatchPOSAddenda = "found and 1 Addenda02 is required for SEC Type POS"
+
+// NewBatchPOS returns a *BatchPOS
+func NewBatchPOS(bh *BatchHeader) *BatchPOS {
+	batch := new(BatchPOS)
+	batch.SetControl(NewBatchControl())
+	batch.SetHeader(bh)
+	return batch
+}
+
+// Validate checks valid NACHA batch rules. Assumes properly parsed records.
+func (batch *BatchPOS) Validate() error {
+	// basic verification of the batch before we validate specific rules.
+	if err := batch.verify(); err != nil {
+		return err
+	}
+	// Add configuration based validation for this type.
+
+	// POS Addenda must be Addenda02
+	if err := batch.isAddenda02(); err != nil {
+		return err
+	}
+
+	// Add type specific validation.
+	if batch.Header.StandardEntryClassCode != "POS" {
+		msg := fmt.Sprintf(msgBatchSECType, batch.Header.StandardEntryClassCode, "POS")
+		return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "StandardEntryClassCode", Msg: msg}
+	}
+
+	//ToDo;  Determine if this is needed
+	/*	// POS detail entries can only be a debit, ServiceClassCode must allow debits
+		switch batch.Header.ServiceClassCode {
+		case 200, 220, 280:
+			msg := fmt.Sprintf(msgBatchServiceClassCode, batch.Header.ServiceClassCode, "POS")
+			return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "ServiceClassCode", Msg: msg}
+		}*/
+
+	for _, entry := range batch.Entries {
+		// POS detail entries must be a debit
+		if entry.CreditOrDebit() != "D" {
+			msg := fmt.Sprintf(msgBatchTransactionCodeCredit, entry.TransactionCode)
+			return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "TransactionCode", Msg: msg}
+		}
+		//ToDo;  Additional validations
+	}
+	return nil
+}
+
+// Create takes Batch Header and Entries and builds a valid batch
+func (batch *BatchPOS) Create() error {
+	// generates sequence numbers and batch control
+	if err := batch.build(); err != nil {
+		return err
+	}
+	// Additional steps specific to batch type
+	// ...
+
+	return batch.Validate()
+}
+
+// isAddenda02 verifies that a Addenda02 exists for each EntryDetail and is Validated
+func (batch *BatchPOS) isAddenda02() error {
+	for _, entry := range batch.Entries {
+		// Addenda type must be equal to 1
+		if len(entry.Addendum) != 1 {
+			return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "Addendum", Msg: msgBatchPOSAddenda}
+		}
+		// Addenda type assertion must be Addenda02
+		addenda02, ok := entry.Addendum[0].(*Addenda02)
+		if !ok {
+			msg := fmt.Sprintf(msgBatchCORAddendaType, entry.Addendum[0])
+			return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "Addendum", Msg: msg}
+		}
+		// Addenda02 must be Validated
+		if err := addenda02.Validate(); err != nil {
+			// convert the field error in to a batch error for a consistent api
+			if e, ok := err.(*FieldError); ok {
+				return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: e.FieldName, Msg: e.Msg}
+			}
+		}
+	}
+	return nil
+}
