11b62e72f44818a1c4f6b332ecb44dc4a942a312
Brooke E Kline Jr <brooke.kline@moov.io>
2018-07-31 09:47:43 -0400

    #212 Write Test
    #212 Write Test

---
+++ test/ach-ctx-write/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/bkmoovio/ach"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// Example transfer to write an ACH CTX file to send/credit a external institutions account
+	// Important: All financial institutions are different and will require registration and exact field values.
+
+	// Set originator bank ODFI and destination Operator for the financial institution
+	// this is the funding/receiving source of the transfer
+	fh := ach.NewFileHeader()
+	fh.ImmediateDestination = "231380104" // Routing Number of the ACH Operator or receiving point to which the file is being sent
+	fh.ImmediateOrigin = "121042882"      // Routing Number of the ACH Operator or sending point that is sending the file
+	fh.FileCreationDate = time.Now()      // Today's Date
+	fh.ImmediateDestinationName = "Federal Reserve Bank"
+	fh.ImmediateOriginName = "My Bank Name"
+
+	// BatchHeader identifies the originating entity and the type of transactions contained in the batch
+	bh := ach.NewBatchHeader()
+	bh.ServiceClassCode = 220          // ACH credit pushes money out, 225 debits/pulls money in.
+	bh.CompanyName = "Name on Account" // The name of the company/person that has relationship with receiver
+	bh.CompanyIdentification = fh.ImmediateOrigin
+	bh.StandardEntryClassCode = "CTX"      // Consumer destination vs Company CCD
+	bh.CompanyEntryDescription = "ACH CTX" // will be on receiving accounts statement
+	bh.EffectiveEntryDate = time.Now().AddDate(0, 0, 1)
+	bh.ODFIIdentification = "121042882" // Originating Routing Number
+
+	// Identifies the receivers account information
+	// can be multiple entry's per batch
+	entry := ach.NewEntryDetail()
+	// Identifies the entry as a debit and credit entry AND to what type of account (Savings, DDA, Loan, GL)
+	entry.TransactionCode = 27          // Code 22: Credit to Store checking account
+	entry.SetRDFI("231380104")          // Receivers bank transit routing number
+	entry.DFIAccountNumber = "12345678" // Receivers bank account number
+	entry.Amount = 100000000            // Amount of transaction with no decimal. One dollar and eleven cents = 111
+	entry.IdentificationNumber = "45689033"
+	entry.SetCTXAddendaRecords(2)
+	entry.SetCTXReceivingCompany("Receiver Company")
+	entry.SetTraceNumber(bh.ODFIIdentification, 1)
+	entry.DiscretionaryData = "01"
+
+	addenda1 := ach.NewAddenda05()
+	addenda1.PaymentRelatedInformation = "Debit First Account"
+	addenda1.SequenceNumber = 1
+	addenda1.EntryDetailSequenceNumber = 0000001
+
+	addenda2 := ach.NewAddenda05()
+	addenda2.PaymentRelatedInformation = "Debit Second Account"
+	addenda2.SequenceNumber = 2
+	addenda2.EntryDetailSequenceNumber = 0000001
+
+	// build the batch
+	batch := ach.NewBatchCTX(bh)
+	batch.AddEntry(entry)
+	batch.GetEntries()[0].AddAddenda(addenda1)
+	batch.GetEntries()[0].AddAddenda(addenda2)
+	if err := batch.Create(); err != nil {
+		log.Fatalf("Unexpected error building batch: %s\n", err)
+	}
+
+	// build the file
+	file := ach.NewFile()
+	file.SetHeader(fh)
+	file.AddBatch(batch)
+	if err := file.Create(); err != nil {
+		log.Fatalf("Unexpected error building file: %s\n", err)
+	}
+
+	// write the file to std out. Anything io.Writer
+	w := ach.NewWriter(os.Stdout)
+	if err := w.Write(file); err != nil {
+		log.Fatalf("Unexpected error: %s\n", err)
+	}
+	w.Flush()
+}
