a17e4a7af3f972bf45047f57977be9300d4c4a5a
Adam Shannon <adamkshannon@gmail.com>
2018-09-04 18:45:49 -0500

    server: report timings in milliseconds

--- server/middlewear.go
+++ server/middlewear.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/go-kit/kit/log"
@@ -24,60 +25,95 @@ type loggingMiddleware struct {
 	logger log.Logger
 }
 
+// timer represents a one-time stoppable time tracker
+// it's assumed startTimer() is called to initialize a timer
+type timer struct {
+	start, end time.Time
+}
+func startTimer() *timer {
+	return &timer{
+		start: time.Now(),
+	}
+}
+// stop returns the number of milliseconds for the given timer
+// and stops the timer.
+func (t *timer) stop() int64 {
+	when := time.Now()
+	if t.end.IsZero() {
+		t.end = when
+	}
+	return t.end.Sub(t.start).Nanoseconds()/1e6
+}
+func (t *timer) String() string {
+	return fmt.Sprintf("%dms", t.stop())
+}
+
+// Middleware endpoints
+
 func (mw loggingMiddleware) CreateFile(f ach.FileHeader) (id string, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "CreateFile", "id", f.ID, "took", time.Since(begin), "err", err)
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		// f.ID can be empty here if the request does not specify it, do we care?
+		// service's CreateFile generates and assigns one just fine
+		mw.logger.Log("method", "CreateFile", "id", f.ID, "took", t, "err", err)
+	}()
 	return mw.next.CreateFile(f)
 }
 
 func (mw loggingMiddleware) GetFile(id string) (f ach.File, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetFile", "id", id, "took", time.Since(begin), "err", err)
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "GetFile", "id", id, "took", t, "err", err)
+	}()
 	return mw.next.GetFile(id)
 }
 
 func (mw loggingMiddleware) GetFiles() []ach.File {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetFiles", "took", time.Since(begin))
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "GetFiles", "took", t)
+	}()
 	return mw.next.GetFiles()
 }
 
 func (mw loggingMiddleware) DeleteFile(id string) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "DeleteFile", "id", id, "took", time.Since(begin))
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "DeleteFile", "id", id, "took", t)
+	}()
 	return mw.next.DeleteFile(id)
 }
 
 //** BATCHES ** //
 
 func (mw loggingMiddleware) CreateBatch(fileID string, bh ach.BatchHeader) (id string, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "CreateBatch", "FileID", fileID, "batchID", bh.ID, "took", time.Since(begin), "err", err)
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "CreateBatch", "FileID", fileID, "batchID", bh.ID, "took", t, "err", err)
+	}()
 	return mw.next.CreateBatch(fileID, bh)
 }
 
 func (mw loggingMiddleware) GetBatch(fileID string, batchID string) (b ach.Batcher, err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetBatch", "fileID", fileID, "batchID", batchID, "took", time.Since(begin), "err", err)
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "GetBatch", "fileID", fileID, "batchID", batchID, "took", t, "err", err)
+	}()
 	return mw.next.GetBatch(fileID, batchID)
 }
 
 func (mw loggingMiddleware) GetBatches(fileID string) []ach.Batcher {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "GetBatches", "fileID", fileID, "took", time.Since(begin))
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "GetBatches", "fileID", fileID, "took", t)
+	}()
 	return mw.next.GetBatches(fileID)
 }
 
 func (mw loggingMiddleware) DeleteBatch(fileID string, batchID string) (err error) {
-	defer func(begin time.Time) {
-		mw.logger.Log("method", "DeleteBatch", "fileID", fileID, "batchID", batchID, "took", time.Since(begin), "err", err)
-	}(time.Now())
+	t := startTimer()
+	defer func() {
+		mw.logger.Log("method", "DeleteBatch", "fileID", fileID, "batchID", batchID, "took", t, "err", err)
+	}()
 	return mw.next.DeleteBatch(fileID, batchID)
 }
