b2f4ead1a53cff65385e5fb47939c4e8e8e9d048
Adam Shannon <adamkshannon@gmail.com>
2018-09-06 13:07:19 -0700

    build: add Dockerfile and makefile

---
+++ version.go
@@ -0,0 +1,7 @@
+// Copyright 2018 The ACH Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package ach
+
+const Version = "v0.3.0-dev"
