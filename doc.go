9b0c1cfd9fd4f82ee305c2cea1ba91471ce26547
Adam Shannon <adamkshannon@gmail.com>
2018-09-06 15:40:59 -0700

    doc: move project level doc header to doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2018 The ACH Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+// Package ach reads and writes (ACH) Automated Clearing House files. ACH is the
+// primary method of electronic money movement through the United States.
+//
+// https://en.wikipedia.org/wiki/Automated_Clearing_House
+package ach
