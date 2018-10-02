23d6047dd86d77707f05ee41b86b3fb6a132daed
Brooke E Kline Jr <brooke.kline@moov.io>
2018-10-02 11:13:17 -0400

    ACH Moov Authors
    ACH Moov Authors

--- addenda05_test.go
+++ addenda05_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The ACH Authors
+// Copyright 2018 The Moov Authors
 // Use of this source code is governed by an Apache License
 // license that can be found in the LICENSE file.
 
