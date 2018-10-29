0c0aa90855c7a5613509b8adbe3f64c5bcb5dc4c
Adam Shannon <adamkshannon@gmail.com>
2018-10-29 12:41:31 -0500

    server: drop content-length check on create file
    This was throwing problems and we should approach this in a uniform
    across all our apps. I'm thinking something along the LB tier.

--- server/routing_test.go
+++ server/routing_test.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"encoding/json"
-	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
@@ -47,24 +46,6 @@ func TestEncodeTextResponse(t *testing.T) {
 
 	if v := w.Header().Get("content-type"); v != "text/plain" {
 		t.Errorf("got %q", v)
-	}
-}
-
-func TestAcceptableContentLength(t *testing.T) {
-	h := make(http.Header)
-
-	if acceptableContentLength(h) { // reject if missing header
-		t.Error("wanted unacceptable")
-	}
-
-	h.Set("Content-Length", "1000")
-	if !acceptableContentLength(h) {
-		t.Error("should have accepted")
-	}
-
-	h.Set("Content-Length", "10000000000000")
-	if acceptableContentLength(h) {
-		t.Error("should have rejected")
 	}
 }
 
