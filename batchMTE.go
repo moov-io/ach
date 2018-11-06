8518f773b24566602e09d8e661832190dc3e4fef
Adam Shannon <adamkshannon@gmail.com>
2018-11-06 15:21:22 -0600

    Add batchMTE for Machine Transfer Entry
    Fixes: https://github.com/moov-io/ach/issues/344

---
+++ batchMTE.go
@@ -0,0 +1,83 @@
+// Copyright 2018 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package ach
+
+import (
+	"fmt"
+
+	"github.com/moov-io/ach/internal/usabbrev"
+)
+
+// BatchMTE holds the BatchHeader, BatchControl, and EntryDetail for Machine Transfer Entry (MTE) entries.
+//
+// A MTE transaction is created when a consumer uses their debit card at an Automated Teller Machine (ATM) to withdraw cash.
+// MTE transactions cannot be aggregated together under a single Entry.
+type BatchMTE struct {
+	batch
+}
+
+func NewBatchMTE(bh *BatchHeader) *BatchMTE {
+	batch := new(BatchMTE)
+	batch.SetControl(NewBatchControl())
+	batch.SetHeader(bh)
+	return batch
+}
+
+// Company Name - Identifies owner of terminal where transaction was initiated (merchant where ATM physically is) // TODO(Adam): what field?
+//
+// Individual Identification: required (non-blank, non-zero)
+//  - Field 8 is used? (Instead of field 7)
+//
+// CompanyDescriptiveDate: actual date of transfer (YYMMDD)
+
+// IAT Addenta (if first is MTE and TransactionCode == MTE (or POS/SHR))
+// TERMINAL IDENTIFICATION CODE * TERMINAL LOCATION * TERMINAL CITY * TERMINAL STATE / FOREIGN COUNTRY
+//  (max 6 chars)                   (max 27 chars)     (max 15 chars)       (max 2 chars)
+//
+// Example: 200509*321 East Market Street*Anytown*VA\
+//          367802*10TH & VINE STREETS*LONDON*UK\
+//
+// TODO(adam): "Starting at position 4" ...  (spaces?)
+// TODO(adam): needs to be all caps?
+
+// Validate checks valid NACHA batch rules. Assumes properly parsed records.
+func (batch *BatchMTE) Validate() error {
+	// basic verification of the batch before we validate specific rules.
+	if err := batch.verify(); err != nil {
+		return err
+	}
+
+	if batch.Header.StandardEntryClassCode != "MTE" {
+		msg := fmt.Sprintf(msgBatchSECType, batch.Header.StandardEntryClassCode, "MTE")
+		return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "StandardEntryClassCode", Msg: msg}
+	}
+
+	for _, entry := range batch.Entries {
+		if entry.Amount <= 0 {
+			msg := fmt.Sprintf(msgBatchAmountNonZero, entry.Amount, "MTE")
+			return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "Amount", Msg: msg}
+		}
+
+		if !usabbrev.Valid(entry.Addenda02.TerminalState) {
+			msg := fmt.Sprintf("%q is not a valid US state or territory", entry.Addenda02.TerminalState)
+			return &BatchError{BatchNumber: batch.Header.BatchNumber, FieldName: "TerminalState", Msg: msg}
+		}
+
+		// Verify Addenda* FieldInclusion based on entry.Category and batchHeader.StandardEntryClassCode
+		if err := batch.addendaFieldInclusion(entry); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Create takes Batch Header and Entries and builds a valid batch
+func (batch *BatchMTE) Create() error {
+	// generates sequence numbers and batch control
+	if err := batch.build(); err != nil {
+		return err
+	}
+	return batch.Validate()
+}
