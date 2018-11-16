9d5501dc173ed868982e9ac3e345063fa5c098c1
Brooke E Kline Jr <brooke.kline@moov.io>
2018-11-16 16:09:35 -0500

    ValidTranCodeForServiceClassCode
    Add ValidTranCodeForServiceClassCode in batch

--- batcher.go
+++ batcher.go
@@ -63,6 +63,7 @@ var (
 	msgBatchRequiredAddendaCount    = "%v addendum found where %v is required for batch type %v"
 	msgBatchTransactionCodeCredit   = "%v a credit is not allowed"
 	msgBatchSECType                 = "header SEC type code %v for batch type %v"
+	msgBatchServiceClassTranCode    = "%v is not valid for %v"
 	msgBatchServiceClassCode        = "Service Class Code %v is not valid for batch type %v"
 	msgBatchForwardReturn           = "Forward and Return entries found in the same batch"
 	msgBatchAmount                  = "Amount must be less than %v for SEC code %v"
