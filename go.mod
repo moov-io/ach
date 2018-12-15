286897422d40a0d3fdb48f45c63d88639fd79159
Adam Shannon <adamkshannon@gmail.com>
2018-12-14 18:36:43 -0600

    vendor: update dependencies

--- go.mod
+++ go.mod
@@ -2,21 +2,12 @@ module github.com/moov-io/ach
 
 require (
 	github.com/VividCortex/gohistogram v1.0.0 // indirect
-	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
-	github.com/go-kit/kit v0.7.0
-	github.com/go-logfmt/logfmt v0.3.0 // indirect
+	github.com/go-kit/kit v0.8.0
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
-	github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/moov-io/base v0.0.0-20181203201558-b27bcdd66705
-	github.com/prometheus/client_golang v0.8.0
-	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
-	github.com/prometheus/common v0.0.0-20180801064454-c7de2306084e // indirect
-	github.com/prometheus/procfs v0.0.0-20180725123919-05ee40e3a273 // indirect
-	golang.org/x/net v0.0.0-20180906233101-161cd47e91fd // indirect
-	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	github.com/moov-io/base v0.0.0-20181214235706-7d7561f2f84d
+	github.com/prometheus/client_golang v0.9.2
 )
---
+++ vendor/github.com/moov-io/base/time.go
@@ -0,0 +1,69 @@
+// Copyright 2018 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package base
+
+import (
+	"time"
+)
+
+const (
+	iso8601Format = "2006-01-02T15:04:05Z07:00"
+)
+
+// Time is an time.Time struct that encodes and decodes in ISO 8601.
+//
+// ISO 8601 is usable by a large array of libraries whereas RFC 3339 support
+// isn't often part of language standard libraries.
+type Time struct {
+	time.Time
+}
+
+// Now returns a Time object with the current clock time set.
+func Now() Time {
+	return Time{
+		Time: time.Now().UTC().Truncate(1 * time.Second),
+	}
+}
+
+// NewTime wraps a time.Time value in Moov's base.Time struct.
+// If you need the underlying time.Time value call .Time:
+//
+// now := Now()
+// fmt.Println(start.Sub(now.Time))
+func NewTime(t time.Time) Time {
+	return Time{t}
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	var bs []byte
+	bs = append(bs, '"')
+
+	t.Time = t.Time.Truncate(1 * time.Second) // drop milliseconds
+	bs = t.AppendFormat(bs, iso8601Format)
+
+	bs = append(bs, '"')
+	return bs, nil
+}
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	// Ignore null, like in the main JSON package.
+	if string(data) == "null" {
+		return nil
+	}
+	if tt, err := time.Parse(`"`+iso8601Format+`"`, string(data)); err != nil {
+		return err
+	} else {
+		t.Time = tt.Truncate(1 * time.Second) // drop millis
+	}
+	return nil
+}
+
+// Equal compares two Time values. Time values are considered equal if they both truncate
+// to the same year/month/day and hour/minute/second.
+func (t Time) Equal(other Time) bool {
+	t1 := t.Time.Truncate(1 * time.Second)
+	t2 := other.Time.Truncate(1 * time.Second)
+	return t1.Equal(t2)
+}
