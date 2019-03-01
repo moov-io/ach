c022959a4bb5c658e11b2c9ed154cbaf219c4fe9
Brooke E Kline Jr <brooke.kline@moov.io>
2019-03-01 12:25:55 -0500

    Update reader_test.go
    don't run for windows

--- test/fuzz-reader/reader_test.go
+++ test/fuzz-reader/reader_test.go
@@ -8,29 +8,34 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 )
 
 func TestCorpusSymlinks(t *testing.T) {
-	fds, err := ioutil.ReadDir("corpus")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(fds) == 0 {
-		t.Fatal("no file descriptors found in corpus/")
-	}
 
-	for i := range fds {
-		if fds[i].Mode()&os.ModeSymlink != 0 {
-			if path, err := os.Readlink(filepath.Join("corpus", fds[i].Name())); err != nil {
-				t.Errorf("broken symlink: %v", err)
-			} else {
-				if _, err := os.Stat(filepath.Join("corpus", path)); err != nil {
+	// avoid symbolic link error on windows
+	if runtime.GOOS != "windows" {
+		fds, err := ioutil.ReadDir("corpus")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(fds) == 0 {
+			t.Fatal("no file descriptors found in corpus/")
+		}
+
+		for i := range fds {
+			if fds[i].Mode()&os.ModeSymlink != 0 {
+				if path, err := os.Readlink(filepath.Join("corpus", fds[i].Name())); err != nil {
 					t.Errorf("broken symlink: %v", err)
+				} else {
+					if _, err := os.Stat(filepath.Join("corpus", path)); err != nil {
+						t.Errorf("broken symlink: %v", err)
+					}
 				}
+			} else {
+				t.Errorf("%s isn't a symlink, move outside corpus/ and symlink into directory", fds[i].Name())
 			}
-		} else {
-			t.Errorf("%s isn't a symlink, move outside corpus/ and symlink into directory", fds[i].Name())
 		}
 	}
 }
