574da6dbf1f259af129c5a32a31392a4d8cccdf2
Brooke E Kline Jr <brooke.kline@moov.io>
2019-07-23 10:18:37 -0400

    Update segmentFileConfiguration_test.go
    Add test to use mock

--- segmentFileConfiguration_test.go
+++ segmentFileConfiguration_test.go
@@ -4,8 +4,18 @@
 
 package ach
 
+import "testing"
+
 // mockSegmentFileConfiguration creates a Segment File Configuration
 func mockSegmentFileConfiguration() *SegmentFileConfiguration {
 	sfc := NewSegmentFileConfiguration()
 	return sfc
+}
+
+// TestSegmentFileConfiguration validates Segment File Configuration
+func TestSegmentFileConfiguration(t *testing.T) {
+	sfc := mockSegmentFileConfiguration()
+	if sfc == nil {
+		t.Error("mockSegmentFileConfiguration does not validate and will break other tests")
+	}
 }
