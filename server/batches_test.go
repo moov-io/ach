f175020177c0017f002eb3634e222a6deebc893b
Adam Shannon <adamkshannon@gmail.com>
2019-08-08 13:33:18 -0500

    server: add tests for Batch routes

--- server/batches_test.go
+++ server/batches_test.go
@@ -9,11 +9,13 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/moov-io/ach"
+
 	"github.com/go-kit/kit/log"
 )
 
 func TestFiles__createBatchEndpoint(t *testing.T) {
-	repo := NewRepositoryInMemory(testTTLDuration, nil)
+	repo := NewRepositoryInMemory(testTTLDuration, log.NewNopLogger())
 	svc := NewService(repo)
 
 	body := strings.NewReader(`{"random":"json"}`)
@@ -26,10 +28,29 @@ func TestFiles__createBatchEndpoint(t *testing.T) {
 	if err == nil || r.Err == nil {
 		t.Errorf("expected error: err=%v resp.Err=%v", err, r.Err)
 	}
+
+	f := ach.NewFile()
+	f.ID = "create-batch"
+	if err := repo.StoreFile(f); err != nil {
+		t.Fatal(err)
+	}
+
+	// successful batch
+	resp, err = createBatchEndpoint(svc, log.NewNopLogger())(context.TODO(), createBatchRequest{
+		FileID: f.ID,
+		Batch:  &mockBatchWEB().Batch,
+	})
+	if r, ok := resp.(createBatchResponse); ok {
+		if r.ID != "54321" || err != nil {
+			t.Errorf("id=%s error=%v", r.ID, r.Err)
+		}
+	} else {
+		t.Errorf("%T %#v", resp, resp)
+	}
 }
 
 func TestFiles__getBatchesEndpoint(t *testing.T) {
-	repo := NewRepositoryInMemory(testTTLDuration, nil)
+	repo := NewRepositoryInMemory(testTTLDuration, log.NewNopLogger())
 	svc := NewService(repo)
 
 	body := strings.NewReader(`{"random":"json"}`)
@@ -42,10 +63,31 @@ func TestFiles__getBatchesEndpoint(t *testing.T) {
 	if err == nil || r.Err == nil {
 		t.Errorf("expected error: err=%v resp.Err=%v", err, r.Err)
 	}
+
+	// successful batch
+	f := ach.NewFile()
+	f.ID = "get-batches"
+	f.AddBatch(mockBatchWEB())
+	if err := repo.StoreFile(f); err != nil {
+		t.Fatal(err)
+	}
+	resp, err = getBatchesEndpoint(svc, log.NewNopLogger())(context.TODO(), getBatchesRequest{
+		fileID: f.ID,
+	})
+	if r, ok := resp.(getBatchesResponse); ok {
+		if len(r.Batches) != 1 {
+			t.Errorf("got %d Batches=%#v", len(r.Batches), r.Batches)
+		}
+		if err != nil {
+			t.Error(r.Err)
+		}
+	} else {
+		t.Errorf("%T %#v", resp, resp)
+	}
 }
 
 func TestFiles__getBatchEndpoint(t *testing.T) {
-	repo := NewRepositoryInMemory(testTTLDuration, nil)
+	repo := NewRepositoryInMemory(testTTLDuration, log.NewNopLogger())
 	svc := NewService(repo)
 
 	body := strings.NewReader(`{"random":"json"}`)
@@ -58,10 +100,33 @@ func TestFiles__getBatchEndpoint(t *testing.T) {
 	if err == nil || r.Err == nil {
 		t.Errorf("expected error: err=%v resp.Err=%v", err, r.Err)
 	}
+
+	// successful batch
+	f := ach.NewFile()
+	f.ID = "get-batch"
+	b := mockBatchWEB()
+	f.AddBatch(b)
+	if err := repo.StoreFile(f); err != nil {
+		t.Fatal(err)
+	}
+	resp, err = getBatchEndpoint(svc, log.NewNopLogger())(context.TODO(), getBatchRequest{
+		fileID:  f.ID,
+		batchID: b.ID(),
+	})
+	if r, ok := resp.(getBatchResponse); ok {
+		if r.Batch == nil {
+			t.Error("nil ach.Batcher")
+		}
+		if err != nil {
+			t.Error(r.Err)
+		}
+	} else {
+		t.Errorf("%T %#v", resp, resp)
+	}
 }
 
 func TestFiles__deleteBatchEndpoint(t *testing.T) {
-	repo := NewRepositoryInMemory(testTTLDuration, nil)
+	repo := NewRepositoryInMemory(testTTLDuration, log.NewNopLogger())
 	svc := NewService(repo)
 
 	body := strings.NewReader(`{"random":"json"}`)
@@ -73,5 +138,25 @@ func TestFiles__deleteBatchEndpoint(t *testing.T) {
 	}
 	if err == nil || r.Err == nil {
 		t.Errorf("expected error: err=%v resp.Err=%v", err, r.Err)
+	}
+
+	// successful batch
+	f := ach.NewFile()
+	f.ID = "delete-batch"
+	b := mockBatchWEB()
+	f.AddBatch(b)
+	if err := repo.StoreFile(f); err != nil {
+		t.Fatal(err)
+	}
+	resp, err = deleteBatchEndpoint(svc, log.NewNopLogger())(context.TODO(), deleteBatchRequest{
+		fileID:  f.ID,
+		batchID: b.ID(),
+	})
+	if r, ok := resp.(deleteBatchResponse); ok {
+		if err != nil {
+			t.Error(r.Err)
+		}
+	} else {
+		t.Errorf("%T %#v", resp, resp)
 	}
 }
