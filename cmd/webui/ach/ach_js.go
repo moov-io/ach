50da5a3ca33eda050c2d098b80d2e11f506598a4
Mark Alexander <mark@simple.com>
2021-05-18 15:39:39 -0700

    Adds human-readable ACH parsing to the webui

--- cmd/webui/ach/ach_js.go
+++ cmd/webui/ach/ach_js.go
@@ -8,6 +8,7 @@ import (
 	"syscall/js"
 
 	"github.com/moov-io/ach"
+	"github.com/moov-io/ach/cmd/achcli/describe"
 )
 
 func parseACH(input string) (string, error) {
@@ -21,6 +22,14 @@ func parseACH(input string) (string, error) {
 	if err := json.NewEncoder(&buf).Encode(file); err != nil {
 		return "", err
 	}
+	return buf.String(), nil
+}
+
+func parseReadable(file *ach.File) (string, error) {
+	var buf bytes.Buffer
+	// passing nil for the masking Opts to be
+	// consistent with the JSON output that is not masked
+	describe.File(&buf, file, nil)
 	return buf.String(), nil
 }
 
@@ -78,8 +87,31 @@ func printACH() js.Func {
 	})
 }
 
+func printReadable() js.Func {
+	return js.FuncOf(func(_ js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid no of arguments passed"
+		}
+		r := strings.NewReader(args[0].String())
+		file, err := ach.NewReader(r).Read()
+		if err != nil {
+			msg := fmt.Sprintf("unable to parse ach file: %v", err)
+			fmt.Println(msg)
+			return msg
+		}
+		parsed, err := parseReadable(&file)
+		if err != nil {
+			fmt.Printf("unable to convert ach file to readable format %s\n", err)
+			return "There was an error formatting the output"
+		}
+
+		return parsed
+	})
+}
+
 func main() {
 	js.Global().Set("parseACH", prettyPrintJSON())
 	js.Global().Set("parseJSON", printACH())
+	js.Global().Set("parseReadable", printReadable())
 	<-make(chan bool)
 }
