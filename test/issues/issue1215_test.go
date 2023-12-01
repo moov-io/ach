fb2e6f6aa43331cf3ad1baedd61b8c74914052f7
Adam Shannon <adamkshannon@gmail.com>
2023-12-01 09:56:49 -0600

    test/issues: write up scenario for Issue 1312
    Issue: https://github.com/moov-io/ach/issues/1332

--- test/issues/issue1215_test.go
+++ test/issues/issue1215_test.go
@@ -63,3 +63,48 @@ func TestIssue1215(t *testing.T) {
 	require.NoError(t, batch.Create())
 	require.NoError(t, batch.Validate())
 }
+
+func TestIssue1332(t *testing.T) {
+	bh := ach.NewBatchHeader()
+	bh.ServiceClassCode = ach.MixedDebitsAndCredits
+	bh.CompanyName = "WhlBodyFit"
+	bh.CompanyIdentification = "MOOVTESTER"
+	bh.StandardEntryClassCode = ach.CCD
+	bh.CompanyEntryDescription = "PMTCHECK"
+	bh.EffectiveEntryDate = "230411"
+	bh.SettlementDate = "102"
+	bh.ODFIIdentification = "05310055"
+
+	entry := ach.NewEntryDetail()
+	entry.TransactionCode = ach.CheckingReturnNOCDebit
+	entry.RDFIIdentification = "12345678"
+	entry.CheckDigit = "0"
+	entry.DFIAccountNumber = "2345532322345"
+	entry.Amount = 0
+	entry.IdentificationNumber = "               "
+	entry.IndividualName = "Whole Body Fitness    "
+	entry.AddendaRecordIndicator = 1
+	entry.TraceNumber = "053100552202922"
+	entry.Category = ach.CategoryReturn
+
+	addenda99 := ach.NewAddenda99()
+	addenda99.ReturnCode = "R03"
+	addenda99.OriginalTrace = "273976363256887"
+	addenda99.DateOfDeath = ""
+	addenda99.OriginalDFI = "12100035"
+	addenda99.AddendaInformation = ""
+	addenda99.TraceNumber = "053100552202922"
+	entry.Addenda99 = addenda99
+
+	batch := ach.NewBatchCCD(bh)
+	batch.AddEntry(entry)
+	batch.SetValidation(&ach.ValidateOpts{
+		// Needs to be set because we lose all information the original entry was a prenote.
+		// Typically the CompanyEntryDescription is set to PRENOTE, which is carried through
+		// to the return.
+		AllowInvalidAmounts: true,
+	})
+
+	require.NoError(t, batch.Create())
+	require.NoError(t, batch.Validate())
+}
