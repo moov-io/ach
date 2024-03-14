060e3e6a2e49fd510cd6a256dc7178d0847728f6
Adam Shannon <adamkshannon@gmail.com>
2024-03-14 13:33:21 -0500

    feat: add MergeDir

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/prometheus/client_golang v1.19.0
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/net v0.22.0
+	golang.org/x/sync v0.5.0
 	golang.org/x/text v0.14.0
 )
 
--- merge.go
+++ merge.go
@@ -18,9 +18,14 @@
 package ach
 
 import (
+	"context"
 	"fmt"
+	"io/fs"
+	"path/filepath"
+	"sync"
 
 	"github.com/igrmk/treemap/v2"
+	"golang.org/x/sync/errgroup"
 )
 
 const NACHAFileLineLimit = 10000
@@ -31,7 +36,7 @@ const NACHAFileLineLimit = 10000
 // This operation will override batch numbers in each file to ensure they do not collide.
 // The ascending batch numbers will start at 1.
 //
-// Entries with TraceNumbers are allowed in the same file, but must be in separate batches
+// Entries with duplicate TraceNumbers are allowed in the same file, but must be in separate batches
 // and are automatically separated.
 //
 // ADV and IAT Batches and Entries are currently not merged together.
@@ -83,13 +88,12 @@ type Conditions struct {
 // This operation will override batch numbers in each file to ensure they do not collide.
 // The ascending batch numbers will start at 1.
 //
-// Entries with TraceNumbers are allowed in the same file, but must be in separate batches
+// Entries with duplicate TraceNumbers are allowed in the same file, but must be in separate batches
 // and are automatically separated.
 //
 // ADV and IAT Batches and Entries are currently not merged together.
 //
-// Old rules limit files to 10,000 lines (when rendered in their ASCII encoding), which
-// is the default for this function. Use MergeFilesWith for a higher limit.
+// Conditions allows for capping the maximum line length or dollar amount of merged files.
 //
 // File Batches can only be merged if they are unique and routed to and from the same ABA routing numbers.
 func MergeFilesWith(incoming []*File, conditions Conditions) ([]*File, error) {
@@ -103,37 +107,195 @@ func MergeFilesWith(incoming []*File, conditions Conditions) ([]*File, error) {
 	}
 
 	for i := range incoming {
-		outFile := pickOutFile(incoming[i].Header, sorted)
-		if outFile == nil {
-			return nil, fmt.Errorf("finding outfile from incoming[%d]: %w", i, ErrPleaseReportBug)
-		}
-		outFile.validateOpts = outFile.validateOpts.merge(incoming[i].GetValidation())
-
-		for j := range incoming[i].Batches {
-			bh := incoming[i].Batches[j].GetHeader()
-			if bh == nil {
-				return nil, fmt.Errorf("incoming[%d].batch[%d] has nil batchHeader", i, j)
-			}
-
-			entries := incoming[i].Batches[j].GetEntries()
-			for m := range entries {
-				// Find a batch where this entry can fit
-				b := findOutBatch(bh, outFile.batches, entries[m])
-
-				// No batch can hold this EntryDetail so create one
-				if b == nil {
-					b = &batch{
-						header:  *bh,
-						entries: treemap.New[string, *EntryDetail](),
-					}
-					outFile.batches = append(outFile.batches, b)
-				}
-
-				b.entries.Set(entries[m].TraceNumber, entries[m])
-			}
+		err := sorted.add(incoming[i])
+		if err != nil {
+			return nil, err
 		}
 	}
 
+	return convertToFiles(sorted, conditions)
+}
+
+// MergeDir will consolidate a directory of ACH files into as few files as possible.
+// This is useful for optimizing cost and network utilization.
+//
+// This operation will override batch numbers in each file to ensure they do not collide.
+// The ascending batch numbers will start at 1.
+//
+// Entries with duplicate TraceNumbers are allowed in the same file, but must be in separate batches
+// and are automatically separated.
+//
+// ADV and IAT Batches and Entries are currently not merged together.
+//
+// MergeDir is typically more performant than MergeFiles as it reads files concurrently while merging occurs.
+// This has a more stable cpu and memory usage trend over reading all files into memory and then calling MergeFiles.
+//
+// File Batches can only be merged if they are unique and routed to and from the same ABA routing numbers.
+func MergeDir(dir string, conditions Conditions) ([]*File, error) {
+	sorted := &outFile{}
+	var setup sync.Once
+
+	// We've observed the slowest part of MergeDir is reading files from disk and
+	// parsing them into File structs. We want to have a decent buffer of *File
+	// structs that are ready to merge.
+	//
+	// For example we have observed (on an Intel Mac w/ SSD)
+	//    filepath.Walk        50-250µs
+	//    queueFileForMerging  20-250ms
+	//    sorted.add             1-25ms
+	var g errgroup.Group
+	parseWorkers := 50 // active ACH Reader's
+	discoveredPaths := make(chan string, parseWorkers*2)
+	mergableFiles := make(chan *File, parseWorkers*2)
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	// We are going to scan the directory for files to parse and merge.
+	g.Go(func() error {
+		defer func() {
+			// After we're done reading paths close the channel
+			cancelFunc()
+			close(discoveredPaths)
+		}()
+		return filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+			// For now don't delve into subdirs and bubble up errors
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+
+			if path != "" {
+				discoveredPaths <- path
+			}
+
+			return nil
+		})
+	})
+
+	// Setup concurrent ACH file parsers which is typically the longest part of merging.
+	var wg sync.WaitGroup
+	wg.Add(parseWorkers)
+	for i := 0; i < parseWorkers; i++ {
+		g.Go(func() error {
+			defer wg.Done()
+
+			return queueFileForMerging(ctx, discoveredPaths, setup, sorted, mergableFiles)
+		})
+	}
+	g.Go(func() error {
+		wg.Wait()
+
+		// Sending a nil file is the signal to stop merging
+		mergableFiles <- nil
+		close(mergableFiles)
+
+		return nil
+	})
+
+	// Merge ACH files into the final output
+	g.Go(func() error {
+		for {
+			file := <-mergableFiles
+			if file == nil {
+				return nil
+			}
+
+			// accumulate the file into our merged set
+			err := sorted.add(file)
+			if err != nil {
+				return fmt.Errorf("adding file into merged set failed: %w", err)
+			}
+		}
+		return nil
+	})
+
+	err := g.Wait()
+	if err != nil {
+		return nil, fmt.Errorf("merging %s failed: %w", dir, err)
+	}
+
+	return convertToFiles(sorted, conditions)
+}
+
+func queueFileForMerging(ctx context.Context, discoveredPaths chan string, setup sync.Once, sorted *outFile, mergableFiles chan *File) error {
+	for {
+		select {
+		case path := <-discoveredPaths:
+			if path == "" {
+				return nil
+			}
+
+			// Read the file
+			file, err := ReadFile(path)
+			if file == nil || err != nil {
+				return fmt.Errorf("reading %s failed: %w", path, err)
+			}
+
+			// Save the first file's header information if it's not already
+			setup.Do(func() {
+				sorted.header = file.Header
+				sorted.validateOpts = file.GetValidation()
+			})
+
+			// Only send non-nil files, once this channel receives a nil file we stop merging
+			if file != nil {
+				mergableFiles <- file
+			}
+
+		case <-ctx.Done():
+			return nil
+		}
+	}
+	return nil
+}
+
+// outFile is a partial ACH file with batches and forms a linked list to additional files
+type outFile struct {
+	header  FileHeader
+	batches []*batch
+
+	validateOpts *ValidateOpts
+
+	next *outFile
+}
+
+func (outf *outFile) add(incoming *File) error {
+	outFile := pickOutFile(incoming.Header, outf)
+	if outFile == nil {
+		return fmt.Errorf("found no outfile: %w", ErrPleaseReportBug)
+	}
+	outFile.validateOpts = outFile.validateOpts.merge(incoming.GetValidation())
+
+	for j := range incoming.Batches {
+		bh := incoming.Batches[j].GetHeader()
+		if bh == nil {
+			return fmt.Errorf("batch[%d] has nil BatchHeader", j)
+		}
+
+		entries := incoming.Batches[j].GetEntries()
+		for m := range entries {
+			// Find a batch where this entry can fit
+			b := findOutBatch(bh, outFile.batches, entries[m])
+
+			// No batch can hold this EntryDetail so create one
+			if b == nil {
+				b = &batch{
+					header:  *bh,
+					entries: treemap.New[string, *EntryDetail](),
+				}
+				outFile.batches = append(outFile.batches, b)
+			}
+
+			b.entries.Set(entries[m].TraceNumber, entries[m])
+		}
+	}
+
+	return nil
+}
+
+func convertToFiles(sorted *outFile, conditions Conditions) ([]*File, error) {
 	var batchNumber int
 
 	var out []*File
@@ -250,16 +412,6 @@ func MergeFilesWith(incoming []*File, conditions Conditions) ([]*File, error) {
 		sorted = sorted.next
 	}
 	return out, nil
-}
-
-// outFile is a partial ACH file with batches and forms a linked list to additional files
-type outFile struct {
-	header  FileHeader
-	batches []*batch
-
-	validateOpts *ValidateOpts
-
-	next *outFile
 }
 
 // batch contains a BatcHeader and tree of entries sorted by TraceNumber, which allows for
